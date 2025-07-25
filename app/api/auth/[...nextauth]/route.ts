2e6130557646ba4950ab063411b203f8b2223c7d
DH-KING <77424212+DH-KING@users.noreply.github.com>
2025-07-25 08:05:27 +0300

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,6 @@ import NextAuth, { AuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
-import TwitterProvider from 'next-auth/providers/twitter';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 
 import prisma from '@/app/libs/prismadb';
@@ -11,6 +10,15 @@ import prisma from '@/app/libs/prismadb';
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      // تم تصحيح أسماء المتغيرات هنا
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
     CredentialsProvider({
       name: 'credentials',
       credentials: {
@@ -18,22 +26,18 @@ export const authOptions: AuthOptions = {
         password: { label: 'Password', type: 'password' },
       },
       async authorize(credentials) {
-        // check if user details are passed
         if (!credentials?.email || !credentials?.password) {
-          throw new Error('Please enter your credentials');
+          throw new Error('Invalid credentials');
         }
 
-        // find user in database
         const user = await prisma.user.findUnique({
           where: { email: credentials.email },
         });
 
-        // check if user exists / hashed password is only there for non social logins
         if (!user || !user?.hashedPassword) {
           throw new Error('Invalid credentials');
         }
 
-        // compare passwords
         const isPasswordCorrect = await bcrypt.compare(
           credentials.password,
           user.hashedPassword
@@ -43,21 +47,8 @@ export const authOptions: AuthOptions = {
           throw new Error('Invalid credentials');
         }
 
-        // if all checks pass, return user
         return user;
       },
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string,
-    }),
-    TwitterProvider({
-      clientId: process.env.TWITTER_ID as string,
-      clientSecret: process.env.TWITTER_SECRET as string,
     }),
   ],
   debug: process.env.NODE_ENV === 'development',
